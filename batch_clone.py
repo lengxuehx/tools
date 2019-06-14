d16538b82a3c4ade0653f060c4de55faca9d8ba5
lengxuehx <lengxuehx@gmail.com>
2019-06-14 11:13:19 +0800

    从gitlab批量克隆项目

---
+++ batch_clone.py
@@ -0,0 +1,16 @@
+import os
+data = []
+for i in range(1, 100):
+    r = requests.get('https://git.example.com/api/v4/projects?private_token=provate_token&per_page=100&page=%s'%i)
+    d = r.json()
+    if r.status_code != 200:
+        print(r.text)
+        break
+    data.extend(d)
+
+
+ for d in data:
+    git_url =  d['ssh_url_to_repo']
+    path_with_namespace = d['path_with_namespace']
+    print(git_url, path_with_namespace)
+    os.system('git clone %s %s'%(git_url, path_with_namespace))
