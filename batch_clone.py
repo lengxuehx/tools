369625377e4a3326da1cfb02f3e2e3ff0467905c
wangxingqi <wangxingqi@xmov.ai>
2019-06-17 17:09:46 +0800

    import error

--- batch_clone.py
+++ batch_clone.py
@@ -1,4 +1,6 @@
 import os
+import requests
+
 data = []
 for i in range(1, 100):
     r = requests.get('https://git.example.com/api/v4/projects?private_token=provate_token&per_page=100&page=%s'%i)
@@ -8,9 +10,8 @@ for i in range(1, 100):
         break
     data.extend(d)
 
-
- for d in data:
-    git_url =  d['ssh_url_to_repo']
+for d in data:
+    git_url = d['ssh_url_to_repo']
     path_with_namespace = d['path_with_namespace']
     print(git_url, path_with_namespace)
     os.system('git clone %s %s'%(git_url, path_with_namespace))
